3b111c6fc4795620561141bc7d89c0cb99ca14a6
Ahmed-202020 <ak812554@gmail.com>
2022-01-28 17:46:18 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,11 @@ $(function(){
    $(".indecators li").on("click" , function(e){
       $(".indecators li").removeClass("active") ; 
       $(this).addClass("active");
-   })
+   });
+   $(".en").on("click" , function(){
+      $("html").attr("dir" , "ltr") ;
+   }) ; 
+   $(".ar").on("click" , function(){
+      $("html").attr("dir" , "rtl") ;
+   }) ; 
 });
